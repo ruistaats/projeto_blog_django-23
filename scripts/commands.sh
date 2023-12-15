ab9a1b44f7815f19d096db9c077ad513b5d6e2f1
ruistaats <rui_staats@hotmail.com>
2023-12-14 21:21:29 -0300

    docker-compose.yml

---
+++ scripts/commands.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# O shell irá encerrar a execução do script quando um comando falhar
+set -e
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..." &
+  sleep 0.1
+done
+
+echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+
+python3 manage.py collectstatic #!/bin/sh
+
+# O shell irá encerrar a execução do script quando um comando falhar
+set -e
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+  sleep 0.1
+done
+
+echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+
+python manage.py collectstatic --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py runserver 0.0.0.0:8000
